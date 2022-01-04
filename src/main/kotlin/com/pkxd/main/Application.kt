c43ec1702def609076fc8b0fd66aae840f6070ef
Thales Nascimento <thales.nascimento@afterverse.com>
2022-01-03 21:18:02 -0300

    chore: merge configuration files into single filet

--- src/main/kotlin/com/pkxd/main/Application.kt
+++ src/main/kotlin/com/pkxd/main/Application.kt
@@ -1,17 +1,36 @@
 package com.pkxd
 
 import io.ktor.application.*
-import com.pkxd.main.config.*
-import com.pkxd.main.config.configureHTTP
-import com.pkxd.main.config.configureRouting
-import com.pkxd.main.config.configureSerialization
+import com.pkxd.main.routes.profileRoutes
+import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
-    configureHTTP()
-    configureRouting()
-    configureSerialization()
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        header("MyCustomHeader")
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(ContentNegotiation) {
+        json()
+    }
+
+    install(StatusPages) {
+
+    }
+
+    routing {
+        profileRoutes()
+    }
 }
--- src/main/kotlin/com/pkxd/main/config/Routing.kt
+++
@@ -1,8 +0,0 @@
-package com.pkxd.main.config
-
-import io.ktor.application.*
-import com.pkxd.main.routes.profileRoutes
-
-fun Application.configureRouting() {
-    profileRoutes()
-}
